bf4ec02317cfca26afcc6cc54ed41bdba88509f9
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-08-17 02:32:33 +0530

    Modify the signin Page

---
+++ src/lib/cloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'dbizvo5lq', 
+    api_key: '778663155491961', 
+    api_secret: 'vr6Sko4y2n4zwAfYfBK1HszsYKo'
+
+});
+
+
+export default  cloudinary ;
