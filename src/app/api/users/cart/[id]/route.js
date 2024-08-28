bd59bb3ea11b7090e2657f39ff35df6b3c69cb27
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-08-28 09:16:18 +0530

    Added Get APi for the Cart

--- src/app/api/users/cart/[id]/route.js
+++ src/app/api/users/cart/[id]/route.js
@@ -89,6 +89,51 @@ export const POST = async (request, { params }) => {
   }
 };
 
+
+//List product to the CArt
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+    console.log('Database connected.');
+
+    const cookieStore = cookies();
+    const authToken = cookieStore.get("userAuthToken");
+    console.log('Auth token:', authToken);
+
+    if (!authToken) {
+      throw new Error("User authentication token is missing.");
+    }
+
+    const decodedToken = jwt.decode(authToken.value);
+    console.log("Decoded token:", decodedToken);
+
+    if (!decodedToken || !decodedToken.id) {
+      throw new Error("Invalid token.");
+    }
+
+    const userId = decodedToken.id;
+    console.log('User ID from token:', userId);
+
+    // Find the cart by userId
+    const cart = await cartModels.findOne({ userId });
+    console.log('Fetched cart for user:', cart);
+
+    if (!cart) {
+      return NextResponse.json({ msg: "Cart not found", items: [], totalPrice: 0 }, { status: 404 });
+    }
+
+    // If the cart is found, return its items and total price
+    return NextResponse.json({ msg: "Cart retrieved successfully", items: cart.items, totalPrice: cart.totalPrice }, { status: 200 });
+  } catch (error) {
+    console.error('Error retrieving cart:', error);
+    return NextResponse.json({ msg: "Error retrieving cart", error: error.message }, { status: 500 });
+  }
+};
+
+
+
+
 // Remove a product from the cart
 export const DELETE = async (request, { params }) => {
   const { id } = params;
