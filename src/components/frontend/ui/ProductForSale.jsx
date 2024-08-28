93120864daf8e22179ac8f28a4521f0083aa4b8e
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-08-29 00:54:43 +0530

    Added some products

--- src/components/frontend/ui/ProductForSale.jsx
+++ src/components/frontend/ui/ProductForSale.jsx
@@ -6,7 +6,7 @@ import { FaRegHeart } from "react-icons/fa";
 import Container from '@/components/utils/Container';
 
 const ProductCard = () => {
-    const [product, setProduct] = useState(null);
+    const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [progress, setProgress] = useState(0);
     const router = useRouter();
@@ -26,8 +26,8 @@ const ProductCard = () => {
         // Fetch the product data from the API
         axios.get('/api/admin/dashboard/product/addProduct')
             .then(response => {
-                const productData = response.data[0]; // Assuming the first product is what you want to display
-                setProduct(productData);
+                const productData = response.data;
+                setProducts(productData);
                 setLoading(false); // Set loading to false when data is fetched
             })
             .catch(error => {
@@ -49,21 +49,14 @@ const ProductCard = () => {
         );
     }
 
-    if (!product) {
-        return <div>Product not found.</div>;
+    if (products.length === 0) {
+        return <div>No products found.</div>;
     }
 
-    const { _id, name, actualPrice, originalPrice, featuredImage } = product;
-
-    // Calculate the percentage off
-    const discountPercentage = Math.round(((originalPrice - actualPrice) / originalPrice) * 100);
-
-    // Handle redirection when the product card is clicked
-    const handleCardClick = () => {
-        router.push(`/product/${_id}`);
+    const handleCardClick = (id) => {
+        router.push(`/product/${id}`);
     };
 
-    // Handle wishlist click
     const handleWishlistClick = () => {
         router.push('/auth/signIn');
     };
@@ -71,40 +64,52 @@ const ProductCard = () => {
     return (
         <Container>
             <div className="mb-6">
-                <h2 className="text-2xl font-bold mb-4 underline text-start">Product On Sale</h2>
+                <h2 className="text-2xl font-bold mb-4 underline text-start">Products On Sale</h2>
             </div>
-            <div 
-                className="relative max-w-sm bg-white shadow-xl rounded-lg overflow-hidden transform transition-transform hover:-translate-y-2 hover:shadow-2xl cursor-pointer"
-                onClick={handleCardClick}
-            >
-                <div className="absolute top-0 left-0 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-br-lg z-10">
-                    Sale
-                </div>
-                <div className="absolute top-2 right-2 z-20">
-                    <button 
-                        onClick={e => {
-                            e.stopPropagation(); // Prevent card click event
-                            handleWishlistClick();
-                        }}
-                        className="text-black  hover:text-red-700 transition-colors"
-                    >
-                        <FaRegHeart size={24} />
-                    </button>
-                </div>
-                <div className="w-full h-48 overflow-hidden">
-                    <img className="w-full h-full object-cover object-center transition-transform duration-300 transform hover:scale-110" 
-                        src={featuredImage} 
-                        alt={name} 
-                    />
-                </div>
-                <div className="p-4 bg-gradient-to-b from-gray-50 to-white">
-                    <h3 className="text-lg font-bold mb-2">{name}</h3>
-                    <div className="flex items-center">
-                        <span className="text-gray-500 line-through mr-2">₹{originalPrice}</span>
-                        <span className="text-green-500 font-bold">₹{actualPrice}</span>
-                        <span className="text-red-500 text-sm ml-2">{discountPercentage}% off</span>
-                    </div>
-                </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {products.map(product => {
+                    const { _id, name, actualPrice, originalPrice, featuredImage } = product;
+                    
+                    // Calculate the percentage off
+                    const discountPercentage = Math.round(((originalPrice - actualPrice) / originalPrice) * 100);
+
+                    return (
+                        <div 
+                            key={_id}
+                            className="relative max-w-sm bg-white shadow-xl rounded-lg overflow-hidden transform transition-transform hover:-translate-y-2 hover:shadow-2xl cursor-pointer"
+                            onClick={() => handleCardClick(_id)}
+                        >
+                            <div className="absolute top-0 left-0 bg-red-500 text-white text-xs font-bold px-2 py-1 rounded-br-lg z-10">
+                                Sale
+                            </div>
+                            <div className="absolute top-2 right-2 z-20">
+                                <button 
+                                    onClick={e => {
+                                        e.stopPropagation(); // Prevent card click event
+                                        handleWishlistClick();
+                                    }}
+                                    className="text-black hover:text-red-700 transition-colors"
+                                >
+                                    <FaRegHeart size={24} />
+                                </button>
+                            </div>
+                            <div className="w-full h-48 overflow-hidden">
+                                <img className="w-full h-full object-cover object-center transition-transform duration-300 transform hover:scale-110" 
+                                    src={featuredImage} 
+                                    alt={name} 
+                                />
+                            </div>
+                            <div className="p-4 bg-gradient-to-b from-gray-50 to-white">
+                                <h3 className="text-lg font-bold mb-2">{name}</h3>
+                                <div className="flex items-center">
+                                    <span className="text-gray-500 line-through mr-2">₹{originalPrice}</span>
+                                    <span className="text-green-500 font-bold">₹{actualPrice}</span>
+                                    <span className="text-red-500 text-sm ml-2">{discountPercentage}% off</span>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </Container>
     );
