65d2a0ab7e9be6c23e496c0e230597f83212cc5d
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-10-18 11:47:50 +0530

    Added some icons

--- src/components/frontend/ui/WhoWeAre.jsx
+++ src/components/frontend/ui/WhoWeAre.jsx
@@ -4,6 +4,8 @@ import Banner1 from '../../../../public/frontend/WhoWeAre/Banner1.webp';
 import Banner2 from '../../../../public/frontend/WhoWeAre/Banner2.webp';
 import Banner3 from '../../../../public/frontend/WhoWeAre/Banner3.webp';
 import Banner4 from '../../../../public/frontend/WhoWeAre/Banner4.webp';
+import { MdDirections } from "react-icons/md";
+
 
 const banners = [
   { src: Banner1, label: 'Clean Ingredients' , description: 'By combining the best of nature’s nutrients with science-backed formulas, we create clean, effective supplements tailored to your unique needs.' , heading: 'We Test' },
@@ -35,7 +37,10 @@ const WhoWeAre = () => {
             <div className="absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center text-white p-4">
               <p className="mb-4 text-center font-semibold">{banner.heading}</p>
               <p className="mb-4 text-center">{banner.description}</p>
-              <button className="text-black bg-white px-3 py-2 rounded-2xl text-lg font-bold mt-[4rem]">Learn More</button>
+              <button className="text-black bg-white px-3 py-2 rounded-2xl text-lg font-bold mt-[4rem] flex items-center justify-center gap-2">
+                  Learn More
+                  <MdDirections className="text-xl" />
+                </button>
             </div>
           </div>
         ))}
