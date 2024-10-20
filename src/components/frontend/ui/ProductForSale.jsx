9238526f4737329da7665d33f55f8f87be813bd9
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-10-21 01:52:26 +0530

    Added mobile ui responsive

--- src/components/frontend/ui/ProductForSale.jsx
+++ src/components/frontend/ui/ProductForSale.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { FaRegHeart } from "react-icons/fa";
 import Image from 'next/image';
+import Loader from '@/components/loader/loader';
 
 const ProductCard = () => {
     const [products, setProducts] = useState([]);
@@ -35,19 +36,16 @@ const ProductCard = () => {
 
     if (loading) {
         return (
-            <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
-                <div className="relative w-24 h-24 bg-gray-200 rounded-full border border-gray-300">
-                    <div className="absolute inset-0 flex items-center justify-center text-blue-500 font-bold text-xl">
-                        {progress}%
-                    </div>
-                </div>
-            </div>
+            <Loader/>
         );
     }
 
     if (products.length === 0) {
         return <div>No products found.</div>;
     }
+    const truncateText = (text, limit) => {
+        return text.length > limit ? `${text.substring(0, limit)}...` : text;
+      };
 
     const handleCardClick = (id) => {
         router.push(`/product/${id}`);
@@ -66,46 +64,47 @@ const ProductCard = () => {
 
                     return (
                         <div
-                            key={_id}
-                            className="relative h-[18rem] w-[14rem] sm:h-[22rem] sm:w-[18rem] md:h-[28rem] md:w-[22rem] overflow-hidden rounded-3xl group" // Added group class
-                            onClick={() => handleCardClick(_id)}
-                        >
-                            <div className="absolute top-0 left-0 bg-red-500 text-white text-xs font-bold px-4 py-2 rounded-br-lg z-10">
-                                Sale
-                            </div>
-                            <div className="absolute top-2 right-4 z-20">
-                                <button
-                                    onClick={e => {
-                                        e.stopPropagation();
-                                        handleWishlistClick();
-                                    }}
-                                    className="text-black font-bold hover:text-red-700 transition-colors"
-                                >
-                                    <FaRegHeart size={24} />
-                                </button>
-                            </div>
-
-                            {/* Image */}
-                            <div className="relative h-[18rem] w-[14rem] sm:h-[22rem] sm:w-[18rem] md:h-[28rem] md:w-[22rem] overflow-hidden rounded-3xl">
-                                <img
-                                    className="object-fill w-full h-full transition-transform duration-300 ease-in-out transform group-hover:scale-105" // Apply hover scaling on the group
-                                    src={featuredImage}
-                                    alt={name}
-                                />
-                                <h3 className="absolute bottom-7 left-5 text-center bg-opacity-50 text-red-500 text-lg sm:text-xl md:text-xl font-medium p-1 bg-white px-5 py-2 rounded-lg">{name}</h3>
-                                <div className="absolute bottom-7 right-5 text-center bg-opacity-50 text-red-500 text-lg sm:text-xl md:text-xl font-medium p-1 bg-white px-5 py-2 rounded-lg">
-                                    <div className='flex flex-col'>
-                                        <span className="text-gray-400 line-through mr-2">₹{originalPrice}</span>
-                                        <span className="text-green-500 font-medium ml-4">₹{salePrice}</span>
-                                    </div>
+                        key={_id}
+                        className="relative h-[22rem] w-full  md:w-[18rem] lg:h-[28rem] lg:w-[22rem] overflow-hidden rounded-3xl group" 
+                        onClick={() => handleCardClick(_id)}
+                    >
+                        <div className= "absolute top-0 left-0 bg-red-500 text-white text-xs font-bold px-4 py-2 rounded-br-lg z-10">
+                            Sale
+                        </div>
+                        <div className="absolute top-2 right-4 z-20">
+                            <button
+                                onClick={e => {
+                                    e.stopPropagation();
+                                    handleWishlistClick();
+                                }}
+                                className="text-black font-bold hover:text-red-700 transition-colors"
+                            >
+                                <FaRegHeart size={24} />
+                            </button>
+                        </div>
+                    
+                        {/* Image */}
+                        <div className="relative h-[22rem] w-full overflow-hidden rounded-3xl border border-gray-300">
+                            <img
+                                className="object-fill w-full h-full transition-transform duration-300 ease-in-out transform group-hover:scale-105" // Apply hover scaling on the group
+                                src={featuredImage}
+                                alt={name}
+                            />
+                            <h3 className="absolute bottom-7 left-5 text-center bg-opacity-50 text-red-500 text-lg sm:text-xl md:text-xl font-medium p-1 bg-white px-5 py-2 rounded-lg"> {truncateText(name, 10)} </h3>
+                            <div className="absolute bottom-7 right-5 text-center bg-opacity-50 text-red-500 text-lg sm:text-xl md:text-xl font-medium p-1 bg-white px-5 py-2 rounded-lg">
+                                <div className='flex flex-col'>
+                                    <span className="text-gray-400 line-through mr-2">₹{originalPrice}</span>
+                                    <span className="text-green-500 font-medium ml-4">₹{salePrice}</span>
                                 </div>
                             </div>
-
-                            {/* Shop Now button (visible on hover) */}
-                            <div className="absolute inset-0  bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                <span className="text-black bg-white px-3 py-2 rounded-2xl text-lg font-bold">Shop Now</span>
-                            </div>
                         </div>
+                    
+                        {/* Shop Now button (visible on hover) */}
+                        <div className="absolute inset-0 bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <span className="text-black bg-white px-3 py-2 rounded-2xl text-lg font-bold">Shop Now</span>
+                        </div>
+                    </div>
+                    
                     );
                 })}
             </div>
