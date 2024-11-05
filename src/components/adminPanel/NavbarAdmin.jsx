1b2d1aeb709459e8eb800d7cec6d1e1ea060fd9b
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-11-05 08:58:30 +0530

    Modify the looks of the admin as for the grocery website

--- src/components/adminPanel/NavbarAdmin.jsx
+++ src/components/adminPanel/NavbarAdmin.jsx
@@ -16,52 +16,60 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`bg-gradient-to-r from-black to-gray-800 text-white dark:bg-gray-900 dark:text-gray-200 transition-colors duration-300`}>
-      <div className="container mx-auto flex justify-between items-center p-4">
-        {/* Logo */}
-        <div className="flex items-center space-x-2">
-          <Image src={logo} alt="Logo" className="w-[45px]" />
-          <span className="text-lg font-bold">Admin Panel</span>
-        </div>
+    <nav className="bg-gradient-to-r from-green-600 to-lime-700 text-white transition-colors duration-300 shadow-lg">
+  <div className="container mx-auto flex justify-between items-center p-4">
+    {/* Logo */}
+    <div className="flex items-center space-x-2">
+      <Image src={logo} alt="Logo" className="w-[45px]" />
+      <span className="text-lg font-bold">Admin Panel</span>
+    </div>
 
-        {/* Hamburger Menu for Mobile View */}
-        <div className="block lg:hidden">
-          <button className="text-2xl hover:text-blue-300 transition-colors duration-200">
-            <IoIosMenu />
-          </button>
-        </div>
+    {/* Hamburger Menu for Mobile View */}
+    <div className="block lg:hidden">
+      <button className="text-2xl hover:text-yellow-400 transition-transform transform hover:scale-110 duration-200">
+        <IoIosMenu />
+      </button>
+    </div>
 
-        {/* Navigation Links */}
-        <div className="hidden lg:flex space-x-6">
-          <a href="#" className="flex items-center space-x-2 px-3 py-2 rounded-lg hover:bg-gray-700 dark:hover:bg-gray-800 transition-colors duration-200">
-            <FaHome className="text-lg" />
-            <span>Dashboard</span>
-          </a>
-          <a href="#" className="flex items-center space-x-2 px-3 py-2 rounded-lg hover:bg-gray-700 dark:hover:bg-gray-800 transition-colors duration-200">
-            <FaCog className="text-lg" />
-            <span>Settings</span>
-          </a>
-          <a href="#" className="flex items-center space-x-2 px-3 py-2 rounded-lg hover:bg-gray-700 dark:hover:bg-gray-800 transition-colors duration-200">
-            <FaUser className="text-lg" />
-            <span>Profile</span>
-          </a>
-        </div>
+    {/* Navigation Links */}
+    <div className="hidden lg:flex space-x-6">
+      <a
+        href="#"
+        className="flex items-center space-x-2 px-4 py-2 rounded-lg bg-gray-800 hover:bg-gray-700 transition-colors duration-300 shadow-md"
+      >
+        <FaHome className="text-lg" />
+        <span>Dashboard</span>
+      </a>
+      <a
+        href="#"
+        className="flex items-center space-x-2 px-4 py-2 rounded-lg bg-gray-800 hover:bg-gray-700 transition-colors duration-300 shadow-md"
+      >
+        <FaCog className="text-lg" />
+        <span>Settings</span>
+      </a>
+      <a
+        href="#"
+        className="flex items-center space-x-2 px-4 py-2 rounded-lg bg-gray-800 hover:bg-gray-700 transition-colors duration-300 shadow-md"
+      >
+        <FaUser className="text-lg" />
+        <span>Profile</span>
+      </a>
+    </div>
 
-        {/* Dark Mode Toggle and User Profile Icon */}
-        <div className="flex items-center space-x-4">
-          <button
-            onClick={toggleDarkMode}
-            className="text-2xl hover:text-yellow-400 dark:hover:text-yellow-300 transition-colors duration-200"
-            aria-label="Toggle dark mode"
-          >
-            {darkMode ? <HiSun /> : <HiMoon />}
-          </button>
-          <FaUserCircle className="text-2xl hover:text-blue-300 transition-colors duration-200" />
-        </div>
-      </div>
+    {/* Dark Mode Toggle and User Profile Icon */}
+    <div className="flex items-center space-x-4">
+      <button
+        onClick={toggleDarkMode}
+        className="text-2xl hover:text-yellow-400 transition-transform transform hover:scale-110 duration-200"
+        aria-label="Toggle dark mode"
+      >
+        {darkMode ? <HiSun /> : <HiMoon />}
+      </button>
+      <FaUserCircle className="text-2xl text-gray-800 transition-transform transform hover:scale-110 duration-200" />
+    </div>
+  </div>
+</nav>
 
-      
-    </nav>
   );
 };
 
--- src/components/adminPanel/SidebarAdmin.jsx
+++ src/components/adminPanel/SidebarAdmin.jsx
@@ -1,19 +1,17 @@
 "use client";
 import React, { useState } from 'react';
-import { FaHome, FaUser, FaCog, FaBuilding, FaPlus, FaShoppingCart, FaListAlt } from 'react-icons/fa';
 import { motion } from 'framer-motion';
-import { TiThMenu } from "react-icons/ti";
+import { FaAppleAlt, FaCarrot, FaShoppingCart, FaListUl, FaCog } from "react-icons/fa";
+import { GiReturnArrow, GiTruck, GiFruitBowl, GiOpenBook } from "react-icons/gi";
+import { MdOutlineLogout } from "react-icons/md";
 import { ImProfile } from "react-icons/im";
 import Link from 'next/link';
-import { MdOutlineLogout } from "react-icons/md";
-import { GrArticle } from "react-icons/gr";
 import { useRouter } from 'next/navigation';
 import waveNav from '../../../public/frontend/SvgAssets/wave-nav.svg'; 
 
-
-
 const SidebarAdmin = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
   const router = useRouter();
 
   const toggleSidebar = () => {
@@ -39,91 +37,95 @@ const SidebarAdmin = () => {
   return (
     <div className="flex">
       <motion.div
-        animate={{ width: isOpen ? '250px' : '90px' }}
-        className="bg-gradient-to-r from-black to-gray-800 text-white dark:bg-gray-900 dark:text-gray-200 h-screen p-5 transition-width duration-300 shadow-xl overflow-y-auto"
-        style={{ scrollbarWidth: 'thin', scrollbarColor: 'rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.3)' }}
+        animate={{ width: isOpen ? "250px" : "90px" }}
+        className="bg-green-700 text-white h-screen p-5 transition-width duration-300 shadow-lg overflow-y-auto"
+        style={{ scrollbarWidth: "thin", scrollbarColor: "rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.3)" }}
       >
         <button
           onClick={toggleSidebar}
-          className="bg-white text-black p-2 rounded mb-4 shadow-md transition-transform transform hover:scale-110"
+          className="bg-white text-green-700 p-2 rounded mb-4 shadow-md transition-transform transform hover:scale-110"
         >
-          <TiThMenu />
+          <FaListUl />
         </button>
-        <div className={`flex flex-col space-y-4`}>
-          <h2 className={`text-lg font-semibold mb-4 ${isOpen ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}>
-            {isOpen ? 'Dashboard' : ''}
+        <div className="flex flex-col space-y-4">
+          <h2 className={`text-lg font-semibold mb-4 ${isOpen ? "opacity-100" : "opacity-0"} transition-opacity duration-300`}>
+            {isOpen ? "Dashboard" : ""}
           </h2>
           <Link href="/admin/dashboard/property/AddProperty" passHref>
-            <SidebarItem icon={<FaHome />} label="Home" isOpen={isOpen} />
+            <SidebarItem icon={<FaAppleAlt />} label="Home" isOpen={isOpen} selected={selectedItem === 'Home'} onClick={() => setSelectedItem('Home')} />
           </Link>
-
 
           {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-500">Orders</h3>}
           <Link href="/admin/dashboard/orders/allOrders" passHref>
-            <SidebarItem icon={<FaShoppingCart />} label="Orders" isOpen={isOpen} />
+            <SidebarItem icon={<FaShoppingCart />} label="All Orders" isOpen={isOpen} selected={selectedItem === 'All Orders'} onClick={() => setSelectedItem('All Orders')} />
           </Link>
-          <Link href="/admin/dashboard/orders/allOrders" passHref>
-            <SidebarItem icon={<FaShoppingCart />} label="Pending Orders" isOpen={isOpen} />
+          <Link href="/admin/dashboard/orders/pendingOrders" passHref>
+            <SidebarItem icon={<FaShoppingCart />} label="Pending Orders" isOpen={isOpen} selected={selectedItem === 'Pending Orders'} onClick={() => setSelectedItem('Pending Orders')} />
           </Link>
 
-          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-300 ">Products</h3>}
-
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Products</h3>}
           <Link href="/admin/dashboard/product/addProduct" passHref>
-            <SidebarItem icon={<FaPlus />} label="Add Product" isOpen={isOpen} />
+            <SidebarItem icon={<GiFruitBowl />} label="Add Product" isOpen={isOpen} selected={selectedItem === 'Add Product'} onClick={() => setSelectedItem('Add Product')} />
           </Link>
           <Link href="/admin/dashboard/product/allProduct" passHref>
-            <SidebarItem icon={<FaBuilding />} label="Products" isOpen={isOpen} />
+            <SidebarItem icon={<FaCarrot />} label="Products" isOpen={isOpen} selected={selectedItem === 'Products'} onClick={() => setSelectedItem('Products')} />
           </Link>
 
-          
-
-          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-300">Categories</h3>}
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Categories</h3>}
           <Link href="/admin/dashboard/category/addCategory" passHref>
-            <SidebarItem icon={<FaPlus />} label="Add Categories" isOpen={isOpen} />
+            <SidebarItem icon={<FaListUl />} label="Add Categories" isOpen={isOpen} selected={selectedItem === 'Add Categories'} onClick={() => setSelectedItem('Add Categories')} />
           </Link>
           <Link href="/admin/dashboard/category/allCategory" passHref>
-            <SidebarItem icon={<FaListAlt />} label="All Categories" isOpen={isOpen} />
+            <SidebarItem icon={<FaListUl />} label="All Categories" isOpen={isOpen} selected={selectedItem === 'All Categories'} onClick={() => setSelectedItem('All Categories')} />
           </Link>
 
-          
-          
-
-          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-300">Blog</h3>}
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Blog</h3>}
           <Link href="/admin/dashboard/blog/addBlog" passHref>
-            <SidebarItem icon={<FaPlus />} label="Add Blog" isOpen={isOpen} />
+            <SidebarItem icon={<GiOpenBook />} label="Add Blog" isOpen={isOpen} selected={selectedItem === 'Add Blog'} onClick={() => setSelectedItem('Add Blog')} />
           </Link>
           <Link href="/admin/dashboard/blog/allBlog" passHref>
-            <SidebarItem icon={<GrArticle />} label="Blogs" isOpen={isOpen} />
+            <SidebarItem icon={<GiOpenBook />} label="Blogs" isOpen={isOpen} selected={selectedItem === 'Blogs'} onClick={() => setSelectedItem('Blogs')} />
           </Link>
 
-          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-300">Policies</h3>}
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Users</h3>}
+          <Link href="/admin/dashboard/user/tableView" passHref>
+            <SidebarItem icon={<FaAppleAlt />} label="Table View" isOpen={isOpen} selected={selectedItem === 'Table View'} onClick={() => setSelectedItem('Table View')} />
+          </Link>
+          <Link href="/admin/dashboard/user/cardView" passHref>
+            <SidebarItem icon={<FaAppleAlt />} label="Card View" isOpen={isOpen} selected={selectedItem === 'Card View'} onClick={() => setSelectedItem('Card View')} />
+          </Link>
+          <Link href="/admin/dashboard/user/search" passHref>
+            <SidebarItem icon={<FaAppleAlt />} label="Search Users" isOpen={isOpen} selected={selectedItem === 'Search Users'} onClick={() => setSelectedItem('Search Users')} />
+          </Link>
+
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Policies</h3>}
           <Link href="/admin/dashboard/policy/termsAndCondition" passHref>
-            <SidebarItem icon={<GrArticle />} label="Terms&Cond.." isOpen={isOpen} />
+            <SidebarItem icon={<GiReturnArrow />} label="Terms & Conditions" isOpen={isOpen} selected={selectedItem === 'Terms & Conditions'} onClick={() => setSelectedItem('Terms & Conditions')} />
           </Link>
           <Link href="/admin/dashboard/policy/privacyPolicy" passHref>
-            <SidebarItem icon={<GrArticle />} label="Privacy Policy" isOpen={isOpen} />
+            <SidebarItem icon={<GiReturnArrow />} label="Privacy Policy" isOpen={isOpen} selected={selectedItem === 'Privacy Policy'} onClick={() => setSelectedItem('Privacy Policy')} />
           </Link>
           <Link href="/admin/dashboard/policy/returnPolicy" passHref>
-            <SidebarItem icon={<GrArticle />} label="Return Policy" isOpen={isOpen} />
+            <SidebarItem icon={<GiReturnArrow />} label="Return Policy" isOpen={isOpen} selected={selectedItem === 'Return Policy'} onClick={() => setSelectedItem('Return Policy')} />
           </Link>
           <Link href="/admin/dashboard/policy/shippingPolicy" passHref>
-            <SidebarItem icon={<GrArticle />} label="Shipping Policy" isOpen={isOpen} />
+            <SidebarItem icon={<GiTruck />} label="Shipping Policy" isOpen={isOpen} selected={selectedItem === 'Shipping Policy'} onClick={() => setSelectedItem('Shipping Policy')} />
           </Link>
-          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-300">Account</h3>}
 
+          {isOpen && <h3 className="text-sm font-medium mt-4 mb-2 text-yellow-400">Account</h3>}
           <Link href="/admin/dashboard/profile" passHref>
-            <SidebarItem icon={<ImProfile />} label="Profile" isOpen={isOpen} />
+            <SidebarItem icon={<ImProfile />} label="Profile" isOpen={isOpen} selected={selectedItem === 'Profile'} onClick={() => setSelectedItem('Profile')} />
           </Link>
           <Link href="/settings" passHref>
-            <SidebarItem icon={<FaCog />} label="Settings" isOpen={isOpen} />
+            <SidebarItem icon={<FaCog />} label="Settings" isOpen={isOpen} selected={selectedItem === 'Settings'} onClick={() => setSelectedItem('Settings')} />
           </Link>
 
           <button
-            className="mt-6 flex items-center rounded-lg bg-red-500 px-4 py-2 text-sm font-medium text-white transition-colors duration-300 hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-500 focus:ring-opacity-50 shadow-md"
+            className="mt-6 flex items-center rounded-lg bg-red-600 px-4 py-2 text-sm font-medium text-white transition-all duration-300 hover:bg-red-700 shadow-md"
             onClick={handleLogout}
           >
             <MdOutlineLogout className="h-5 w-5" aria-hidden="true" />
-            {isOpen && <span>Logout</span>}
+            {isOpen && <span className="ml-2">Logout</span>}
           </button>
         </div>
       </motion.div>
@@ -131,14 +133,17 @@ const SidebarAdmin = () => {
   );
 };
 
-const SidebarItem = ({ icon, label, isOpen }) => {
+const SidebarItem = ({ icon, label, isOpen, selected, onClick }) => {
   return (
     <motion.div
+      onClick={onClick}
       whileHover={{ scale: 1.1 }}
-      className="flex items-center space-x-4 p-2 rounded-lg hover:bg-gray-800 hover:px-5 dark:hover:bg-gray-700 transition-colors duration-300"
+      className={`flex items-center space-x-4 p-2 rounded-lg transition-colors duration-300 ${
+        selected ? "bg-gray-800" : "hover:bg-gray-800"
+      }`}
     >
       <div className="text-xl">{icon}</div>
-      {isOpen && <span className="text-sm font-medium">{label}</span>}
+      {isOpen && <span className="font-medium">{label}</span>}
     </motion.div>
   );
 };
