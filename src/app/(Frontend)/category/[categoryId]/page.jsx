5d0fc27a089c7a71f27ba2600455c3d97b5c781d
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-11-08 14:50:25 +0530

    added subCategory api

--- src/app/(Frontend)/category/[categoryId]/page.jsx
+++ src/app/(Frontend)/category/[categoryId]/page.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 const page = () => {
+
+    const [data, setData] = useState(null); // State to store API response
+    const [loading, setLoading] = useState(true); // State to manage loading state
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const urlPath = window.location.pathname;
+                const id = urlPath.split('/')[2];
+                setIdFromURL(id);
+
+                const response = await fetch(`/api/admin/dashboard/category/subCategory/${id}`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false); 
+            }
+        };
+
+        fetchData();
+    }, []); 
   return (
-    <div>Aa dekhe zra</div>
+
+    <div>
+        
+    </div>
   )
 }
 
