1a701975f79c9c3466f5610bdabc28d49759d514
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-11-09 05:05:04 +0530

    Added subCategories page

--- src/app/(Frontend)/category/[categoryId]/page.jsx
+++ src/app/(Frontend)/category/[categoryId]/page.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Loader from '@/components/loader/loader';
 import React, { useEffect, useState } from 'react';
 
 const Page = () => {
@@ -28,26 +29,28 @@ const Page = () => {
         fetchData();
     }, []);
 
-    if (loading) return <p>Loading...</p>;
+    if (loading) return <Loader />;
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className="p-4">
+        <div className="flex flex-col items-center gap-6 p-4 md:p-8 my-8">
             {/* Display Category Name at the Top */}
-            <h1 className="text-2xl font-bold mb-4">{data?.name || "Category"}</h1>
+            <h1 className="text-lg sm:text-xl md:text-4xl mb-4 font-bold text-red-500">{data?.name || "Category"}</h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                
+            <div className="flex gap-4 sm:gap-6 justify-center p-1 md:p-6 overflow-x-auto snap-x snap-mandatory flex-wrap">
                 {/* Map through subcategories and display each as a card */}
                 {data?.subcategories?.map((subcategory) => (
-                    <div key={subcategory._id} className="border rounded shadow p-4">
+                    <div key={subcategory._id} className="flex-shrink-0 snap-center flex flex-col items-center w-32 h-36 sm:w-36 sm:h-48 md:w-56 md:h-64 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300  sm:p-4 cursor-pointer">
                         {/* Subcategory Image */}
                         <img 
                             src={subcategory.image} 
                             alt={subcategory.name} 
-                            className="w-full h-40 object-cover rounded mb-2"
+                            className="w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 bg-gray-200 rounded-full overflow-hidden flex items-center justify-center mb-2 sm:mb-3"
                         />
                         {/* Subcategory Name */}
-                        <h2 className="text-lg font-semibold">{subcategory.name}</h2>
+                        <h2 className="text-center text-xs sm:text-sm md:text-lg font-medium text-red-500 mt-1 sm:mt-2">{subcategory.name}</h2>
                     </div>
                 ))}
             </div>
