727b996c4f2a5891456e40ca7b6dfd8f8c82b7c3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-01-07 23:51:41 +0530

    feat: add weekday activity, hourly trends, and recent transactions to dashboard, and update overall layout

--- src/app/(AdminPanel)/admin/dashboard/page.jsx
+++ src/app/(AdminPanel)/admin/dashboard/page.jsx
@@ -1,12 +1,12 @@
 import FirstRow from '@/components/adminPanel/dashboard/FirstRow'
-import ThirdRow from '@/components/adminPanel/dashboard/SecondRow'
+import SecondRow from '@/components/adminPanel/dashboard/SecondRow'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='px-4 py-6'>
+    <div className="min-h-screen bg-gray-50/50 p-8 space-y-8">
       <FirstRow/>
-      <ThirdRow/>
+      <SecondRow/>
     </div>
   )
 }
