45353650431c766e14a49445cd2cc7b69f9a5402
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-01-07 23:54:34 +0530

    feat: Implement mock data fallback for donation fetching and refine dashboard layout with controlled scrolling.

--- src/app/(AdminPanel)/admin/dashboard/page.jsx
+++ src/app/(AdminPanel)/admin/dashboard/page.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="min-h-screen bg-gray-50/50 p-8 space-y-8">
-      <FirstRow/>
-      <SecondRow/>
+    <div className="h-[calc(100vh-6rem)] overflow-y-auto bg-gray-50/50 p-8 space-y-8 no-scrollbar">
+      <div className=" mx-auto space-y-8">
+        <FirstRow/>
+        <SecondRow/>
+      </div>
     </div>
   )
 }
